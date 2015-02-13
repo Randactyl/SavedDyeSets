c523616bc4ae3879b3197b5836843452d235a0bb
Randactyl <rafowler@randactyl.com>
2015-02-12 18:49:38 -0800

    finish project refactor and finalize first release

--- SavedDyeSets.lua
+++ SavedDyeSets.lua
@@ -1,32 +1,87 @@
-local SDSData = nil
-
 local control = nil
 local dropdown = nil
 local saveButton = nil
+local saveDialog = nil
 local deleteButton = nil
+local deleteDialog = nil
+local data = nil
+local lang = "en"
+
+local function GetLanguage()
+    local lang = GetCVar("language.2")
+
+    --check for supported languages
+    if SavedDyeSetsStrings[lang] ~= nil then return lang end
+
+    --return english if not supported
+    return "en"
+end
 
 local function DyeStationClosed()
     control:SetHidden(true)
     SLASH_COMMANDS["/dyeset"] = nil
 end
 
+local function LoadDyeSet(name)
+    if data.setTable[name] then
+        for i = 1, 4, 1 do
+            SetSavedDyeSetDyes(i, data.setTable[name]["primary" .. i], data.setTable[name]["secondary" .. i],
+                data.setTable[name]["accent" .. i])
+            DYEING:RefreshSavedSet(i)
+        end
+
+
+        d("Dye sets loaded. (" .. name .. ")")
+    else
+        d("Set does not exist.")
+    end
+end
+
+local function SaveDyeSet(name)
+    for i = 1, 4, 1 do
+        local primaryDyeId, secondaryDyeId, accentDyeId = GetSavedDyeSetDyes(i)
+
+        if data.setTable[name] then
+            
+            data.setTable[name]["primary" .. i] = primaryDyeId
+            data.setTable[name]["secondary" .. i] = secondaryDyeId
+            data.setTable[name]["accent" .. i] = accentDyeId
+        else
+            data.setTable[name] = {}
+            data.setTable[name]["primary" .. i] = primaryDyeId
+            data.setTable[name]["secondary" .. i] = secondaryDyeId
+            data.setTable[name]["accent" .. i] = accentDyeId
+        end
+    end
+
+    d("Dye sets saved. (" .. name .. ")")
+end
+
+local function DeleteDyeSet(name)
+    if data.setTable[name] then
+        data.setTable[name] = nil
+        d("Dye sets deleted. (" .. name .. ")")
+    else
+        d("Set does not exist.")
+    end
+end
+
 local function OnButtonClicked(button)
     if button.save == true then
-        d("save button clicked!")
-        ZO_Dialogs_ShowDialog("SAVE_DYESET")
+        saveDialog = ZO_Dialogs_ShowDialog("SAVE_DYESET")
     else
-        d("delete button clicked!")
-        ZO_Dialogs_ShowDialog("DELETE_DYESET")
+        deleteDialog = ZO_Dialogs_ShowDialog("DELETE_DYESET")
     end
 end
 
 local function OnDropdownSelect(name)
-    SDSData:LoadDyeSet(name)
+    PlaySound(SOUNDS.DIALOG_ACCEPT)
+    LoadDyeSet(name)
 end
 
 local function PopulateDropdown()
     local comboBox = dropdown.m_comboBox
-    local setTable = SDSData:GetSetTable()
+    local setTable = data.setTable
 
     if setTable == nil then return end
 
@@ -55,11 +110,11 @@ local function DyeStationOpened()
             end
         end
 
-        if options[1] == "load" then SDSData:LoadDyeSet(options[2]) end
-        if options[1] == "save" then SDSData:SaveDyeSet(options[2]) end
-        if options[1] == "delete" then SDSData:DeleteDyeSet(options[2]) end
+        if options[1] == "load" then LoadDyeSet(options[2]) end
+        if options[1] == "save" then SaveDyeSet(options[2]) end
+        if options[1] == "delete" then DeleteDyeSet(options[2]) end
         if options[1] == "list" then
-            local setTable = SDSData:GetSetTable()
+            local setTable = data.setTable
 
             for i,_ in pairs(setTable) do
                 d(i)
@@ -108,13 +163,72 @@ local function InitializeControls()
     deleteButton:SetPressedFontColor(0.68627452850342, 0.68627452850342, 0.68627452850342, 1)
     deleteButton:SetClickSound(SOUNDS.DIALOG_ACCEPT)
     deleteButton.delete = true
+
+    local function SetupSaveDialog()
+        local info = {
+            customControl = self,
+            setup = SetupSaveDialog,
+            title = {
+                text = zo_strupper(SavedDyeSetsStrings[lang].SAVE_TITLE),
+            },
+            mainText = {
+                text = SavedDyeSetsStrings[lang].SAVE_TEXT,
+            },
+            editBox = {},
+            buttons = {
+                [1] = {
+                    text = SavedDyeSetsStrings[lang].SAVE_BUTTON,
+                    callback = function() 
+                        SaveDyeSet(ZO_Dialogs_GetEditBoxText(saveDialog))
+                        PopulateDropdown()
+                    end,
+                },
+                [2] = {
+                    text = SI_DIALOG_CANCEL,
+                },
+            },
+        }
+        ZO_Dialogs_RegisterCustomDialog("SAVE_DYESET", info)
+    end
+    local function SetupDeleteDialog()
+        local info = {
+            customControl = self,
+            setup = SetupDeleteDialog,
+            title = {
+                text = zo_strupper(SavedDyeSetsStrings[lang].DELETE_TITLE),
+            },
+            mainText = {
+                text = SavedDyeSetsStrings[lang].DELETE_TEXT,
+            },
+            editBox = {},
+            buttons = {
+                [1] = {
+                    text = SavedDyeSetsStrings[lang].DELETE_BUTTON,
+                    callback = function() 
+                        DeleteDyeSet(ZO_Dialogs_GetEditBoxText(deleteDialog))
+                        PopulateDropdown()
+                    end,
+                },
+                [2] = {
+                    text = SI_DIALOG_CANCEL,
+                }
+            }
+        }
+        ZO_Dialogs_RegisterCustomDialog("DELETE_DYESET", info)
+    end
+
+    SetupSaveDialog()
+    SetupDeleteDialog()
 end
 
 local function SavedDyeSetsOnLoaded(eventCode, addonName)
     if addonName ~= "SavedDyeSets" then return end
     EVENT_MANAGER:UnregisterForEvent("SavedDyeSetsOnLoaded", EVENT_ADD_ON_LOADED)
 
-    SDSData = SavedDyeSetsData:New()
+    local defaults = { setTable = {}, }
+
+    lang = GetLanguage()
+    data = ZO_SavedVars:NewAccountWide("SavedDyeSets_SavedData", 1.1, nil, defaults)
 
     InitializeControls()
 
