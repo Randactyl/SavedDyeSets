f47e34d60dc212b96ac0ea690b04e4eb897ccffe
Randactyl <rafowler@randactyl.com>
2015-02-12 18:48:41 -0800

    add en strings and set metatable fallbacks to en

--- SavedDyeSetsStrings.lua
+++ SavedDyeSetsStrings.lua
@@ -3,7 +3,18 @@ SavedDyeSetsStrings = {
 	["fr"] = {},
 	["en"] = {
 		NAME = "Saved Dye Sets",
+		SAVE_TITLE = "save dye sets", --passed to zo_strupper
+		SAVE_TEXT = "Enter name of set to save:",
+		SAVE_BUTTON = "Save",
+		DELETE_TITLE = "delete dye sets", --passed to zo_strupper
+		DELETE_TEXT = "Enter name of set to delete:",
+		DELETE_BUTTON = "Delete",
 	},
 	["es"] = {},
 	["ru"] = {},
 }
+
+setmetatable(SavedDyeSetsStrings["de"], {__index = SavedDyeSetsStrings["en"]})
+setmetatable(SavedDyeSetsStrings["fr"], {__index = SavedDyeSetsStrings["en"]})
+setmetatable(SavedDyeSetsStrings["ru"], {__index = SavedDyeSetsStrings["en"]})
+setmetatable(SavedDyeSetsStrings["es"], {__index = SavedDyeSetsStrings["en"]})
